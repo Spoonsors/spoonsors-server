7396421da6e173b2c73fd1670c4927df7aafc335
mummhy0811 <mummhy0811@inu.ac.kr>
2023-07-22 18:19:48 +0900

    feat:초기설정

---
+++ src/main/java/com/spoonsors/spoonsorsserver/SpoonsorsserverApplication.java
@@ -0,0 +1,13 @@
+package com.spoonsors.spoonsorsserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpoonsorsserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpoonsorsserverApplication.class, args);
+	}
+
+}
