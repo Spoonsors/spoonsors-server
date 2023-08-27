8548499fddc72e7a861be2cf7a98301665b8c8b6
mummhy0811 <mummhy0811@inu.ac.kr>
2023-08-27 15:47:01 +0900

    Add: 오류처리 기초 틀 작성

---
+++ src/main/java/com/spoonsors/spoonsorsserver/customException/ExceptionEnum.java
@@ -0,0 +1,34 @@
+package com.spoonsors.spoonsorsserver.customException;
+
+import lombok.Getter;
+import lombok.ToString;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@ToString
+public enum ExceptionEnum {
+    RUNTIME_EXCEPTION(HttpStatus.BAD_REQUEST, "E0001"),
+    ACCESS_DENIED_EXCEPTION(HttpStatus.UNAUTHORIZED, "E0002"),
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "E0003"),
+
+    //사용자 정의
+    POST01(HttpStatus.UNAUTHORIZED, "POST-01", "인증이 되지 않은 사용자입니다.");
+
+
+    private final HttpStatus status;
+    private final String code;
+    private String message;
+
+    ExceptionEnum(HttpStatus status, String code) {
+        this.status = status;
+        this.code = code;
+    }
+
+    ExceptionEnum(HttpStatus status, String code, String message) {
+        this.status = status;
+        this.code = code;
+        this.message = message;
+    }
+
+
+}
