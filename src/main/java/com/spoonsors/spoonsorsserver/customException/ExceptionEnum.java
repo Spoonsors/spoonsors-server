9409b8c380e2b27ee51f69eab0c61389761a13c7
mummhy0811 <mummhy0811@inu.ac.kr>
2023-08-29 01:15:10 +0900

    Fix: 글 마감 상태 변경 api 오류처리 수정

--- src/main/java/com/spoonsors/spoonsorsserver/customException/ExceptionEnum.java
+++ src/main/java/com/spoonsors/spoonsorsserver/customException/ExceptionEnum.java
@@ -14,18 +14,24 @@ public enum ExceptionEnum {
     MANAGER01(HttpStatus.FORBIDDEN, "MANAGER-01","이미 등록되어 있는 식재료입니다."),
     MANAGER02(HttpStatus.FORBIDDEN, "MANAGER-02","후원 등록되어 있는 식재료입니다."),
     MANAGER03(HttpStatus.BAD_REQUEST, "MANAGER-03","검색한 식재료가 존재하지 않습니다."),
+
     REVIEW01(HttpStatus.FORBIDDEN, "REVIEW-01","후원 마감되지 않은 글은 리뷰 작성 불가능 합니다."),
+
     OPENAPI01(HttpStatus.BAD_REQUEST, "OPENAPI-01","입력값을 확인해주세요."),
     OPENAPI02(HttpStatus.FORBIDDEN, "OPENAPI-02","유효 호출건수를 이미 초과하셨습니다."),
     OPENAPI03(HttpStatus.BAD_REQUEST, "OPENAPI-03","해당하는 데이터가 없습니다."),
     OPENAPI04(HttpStatus.BAD_REQUEST, "OPENAPI-04","파싱 불가능한 데이터 입니다."),
+
     AUTHORIZE01(HttpStatus.BAD_REQUEST, "AUTHORIZE-01","인증번호가 맞지 않습니다."),
+
     SPON01(HttpStatus.FORBIDDEN, "SPON-01","이미 후원이 완료된 물품입니다."),
+
     POST01(HttpStatus.FORBIDDEN, "POST-01", "인증이 되지 않은 사용자입니다."),
     POST02(HttpStatus.FORBIDDEN, "POST-02", "후원 등록 가능 상태가 아닙니다."),
     POST03(HttpStatus.FORBIDDEN, "POST-03", "후원 상품이 없는 글은 마감 불가능합니다."),
     POST04(HttpStatus.FORBIDDEN, "POST-04", "후원 완료된 글은 삭제 불가능 합니다."),
     POST05(HttpStatus.FORBIDDEN, "POST-05", "후원된 상품이 있는 글은 삭제 불가능 합니다."),
+    POST06(HttpStatus.FORBIDDEN, "POST-06", "리뷰가 있는 글은 상태 변경 불가능 합니다."),
 
     JOIN01(HttpStatus.BAD_REQUEST, "JOIN-01", "이미 존재하는 아이디입니다."),
     JOIN02(HttpStatus.BAD_REQUEST, "JOIN-02", "이미 존재하는 닉네임입니다."),
